a18ee1279502f9a90a25f6a70658e001ed76f0d0
henriquesergio89 <shenrique89.hs@gmail.com>
2024-06-04 08:23:43 -0300

    Movie07: return list of animes with new RequestMapping

---
+++ src/main/java/academy/devdojo/controller/AnimeController.java
@@ -0,0 +1,18 @@
+package academy.devdojo.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = {"v1/animes", "v1/animes/"})
+public class AnimeController {
+
+    @GetMapping
+    public List<String> listAll(){
+        return List.of("Hell's Paradise (Jigokuraku)", "Dr. Stone", "Konosuba");
+    }
+}
---
+++ src/main/java/academy/devdojo/model/Anime.java
@@ -0,0 +1,29 @@
+package academy.devdojo.model;
+
+public class Anime {
+
+    private String id;
+    private String nome;
+
+    public Anime(String id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
